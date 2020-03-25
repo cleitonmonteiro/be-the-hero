b0f4cbc897b0a5f5b20400d1ed7ef6ec3a51a32c
Cleiton Monteiro <cleitonmonteiro.dev@gmail.com>
2020-03-25 19:08:51 -0300

    Rota de sessão para ONGs.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
@@ -13,5 +14,7 @@ routes.post("/incidents", IncidentController.create);
 routes.delete("/incidents/:id", IncidentController.delete);
 
 routes.get("/profile", ProfileController.index);
+
+routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
