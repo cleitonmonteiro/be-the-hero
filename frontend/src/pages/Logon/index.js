c175a498e8e679da759834f46483eab566ae12af
Cleiton Monteiro <cleitonmonteiro.dev@gmail.com>
2020-03-27 10:24:42 -0300

    Gerenciamento de Login.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiLogIn } from "react-icons/fi";
-import {Link} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
-import './styles.css';
+import "./styles.css";
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
+import api from "../../services/api";
 
 export default function Logon() {
-  return(
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+      localStorage.setItem("ongName", response.data.name);
+      localStorage.setItem("ongId", id);
+
+      history.push("profile");
+      
+    } catch (error) {
+      alert("Falhar no login, tente novamente.")
+    }
+  }
+
+  return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be the hero"/>
-        <form>
+        <img src={logoImg} alt="Be the hero" />
+
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input placeholder="Sua ID"/>
-          <button className="button" type="submit">Entrar</button>
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={event => setId(event.target.value)}
+          />
+          <button className="button" type="submit">
+            Entrar
+          </button>
 
           <Link to="/register" className="back-link">
-            <FiLogIn size={16} color="#E02041"/> Não tenho cadastro
+            <FiLogIn size={16} color="#E02041" /> Não tenho cadastro
           </Link>
         </form>
       </section>
-      <img src={heroesImg} alt="Heroes"/>
+      <img src={heroesImg} alt="Heroes" />
     </div>
   );
 }
